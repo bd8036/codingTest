5924cdf77b89be2f0628d4b55294cbb883abdbca
mchoi8036 <mchoi8036@gmail.com>
2021-03-30 21:39:03 +0900

    upload bj1001

---
+++ src/BJ1001.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class BJ1001 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        int firstInt = s.nextInt();
+        int secondInt = s.nextInt();
+
+        System.out.println(firstInt - secondInt);
+    }
+}
